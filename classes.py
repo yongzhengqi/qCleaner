6b4e79783fb6cf7759345d27d0404bf8e9de9934
Qizy <yongzhengqi@gmail.com>
2017-07-15 00:30:50 +0800

    Version 2.0

--- classes.py
+++ classes.py
@@ -42,65 +42,78 @@ class ProcessModule(QDialog):
 
         self.setWindowTitle("正在清理")
         self.setWindowIcon(QIcon("icon.png"))
+        self.resize(300, 200)
 
         self.PathCount = self.FileCount = self.SpaceCount = 0
-        self.CurrentPath = ""
-        self.ForcedTerminated = 0
+        self.UpdateCount = self.ForcedTerminated = 0
 
         self.PathCountStatus = QLabel()
         self.FileCountStatus = QLabel()
         self.SpaceCountStatus = QLabel()
         self.CurrentPathStatus = QLabel()
 
-        button = QPushButton("取消")
-        button.clicked.connect(self.ForcedTermination)
-
-        self.UpdateData()
+        self.ExitButton = QPushButton("取消")
+        self.ExitButton.clicked.connect(self.ForcedTermination)
 
         GridLayout = QGridLayout()
         GridLayout.addWidget(self.CurrentPathStatus, 1, 1)
         GridLayout.addWidget(self.PathCountStatus, 2, 1)
         GridLayout.addWidget(self.FileCountStatus, 3, 1)
         GridLayout.addWidget(self.SpaceCountStatus, 4, 1)
-        GridLayout.addWidget(button, 5, 1)
-
+        GridLayout.addWidget(self.ExitButton, 5, 1)
         self.setLayout(GridLayout)
-        self.resize(300, 200)
-        self.show()
 
-        self.process(path)
-
-        self.CurrentPath = path
-        self.UpdateData()
-
-        button.setText("确认")
-        button.clicked.connect(self.close)
-
-    def UpdateData(self):
-        self.PathCountStatus.setText("已遍历%d个文件夹" % self.PathCount)
-        self.FileCountStatus.setText("已删除%d个.exe文件" % self.FileCount)
-        self.SpaceCountStatus.setText("已释放出%dMB的空间" % (self.SpaceCount / 1024 / 1024))
-        self.CurrentPathStatus.setText("当前目录：%s" % self.CurrentPath)
+        import threading
+        process = threading.Thread(target=self.ProcessGuide, args=(path,))
+        process.start()
 
     def ForcedTermination(self):
         self.ForcedTerminated = 1
 
-    def process(self, path):
+    def ProcessGuide(self, path):
+        self.process(path, 500)
+
+        self.UpdateData("")
+        self.ExitButton.setText("确定")
+        self.ExitButton.clicked.disconnect(self.ForcedTermination)
+        self.ExitButton.clicked.connect(self.close)
+
+        if self.ForcedTerminated:
+            self.setWindowTitle("已取消")
+        else:
+            self.setWindowTitle("清理完成")
+
+    def UpdateData(self, CurrentPath):
+        self.CurrentPathStatus.setText("当前目录：%s" % CurrentPath)
+        self.PathCountStatus.setText("已遍历%d个文件夹" % self.PathCount)
+        self.FileCountStatus.setText("已删除%d个.exe文件" % self.FileCount)
+        self.SpaceCountStatus.setText("已释放出%dMB的空间" % (self.SpaceCount / 1024 / 1024))
+
+    def process(self, path, UpdateCountBase):
         if self.ForcedTerminated:
             return
 
         self.PathCount += 1
-        self.CurrentPath = path
 
-        FileList = os.listdir(path)
+        try:
+            FileList = os.listdir(path)
+        except:
+            return
+
         for file in FileList:
+            self.UpdateCount -= 1
+            if self.UpdateCount <= 0:
+                self.UpdateData(path)
+                self.UpdateCount = UpdateCountBase
+
             AbsolutePath = path + os.sep + file
             if os.path.isdir(AbsolutePath):
-                self.process(AbsolutePath)
+                self.process(AbsolutePath, UpdateCountBase)
             elif os.path.splitext(file)[1] == ".cpp":
                 ExecutableFile = os.path.splitext(file)[0] + ".exe"
                 if (ExecutableFile in FileList):
                     self.SpaceCount += os.path.getsize(path + os.sep + ExecutableFile)
                     self.FileCount += 1
+                    self.UpdateData(path)
 
                     os.remove(path + os.sep + ExecutableFile)
